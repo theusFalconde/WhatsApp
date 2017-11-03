855f205e72a44af06a27060657430ca94b96ac91
Falconde <matheus_felipe25@live.com>
2017-11-02 23:49:31 -0200

    Adicionando Projeto

---
+++ WhatsApp/app/src/main/java/com/developmind/whatsapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.developmind.whatsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference referenciaFirebase = FirebaseDatabase.getInstance().getReference();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        referenciaFirebase.child("pontos").setValue(100);
+    }
+}
